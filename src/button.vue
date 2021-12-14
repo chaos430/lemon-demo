e7ad0954366ca1248d3fe4d6858f260ec3a782c8
chaos430 <1192318530@qq.com>
2021-12-14 16:19:14 +0800

    加入parcel将button组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="l-button">按钮</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.l-button{
+  height: var(--button-height);
+  padding: 0 1em;
+  font-size: var(--font-size);
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover{
+    border-color:var(--border-color-hover);
+  }
+  &:active{
+    background-color: var(--button-active-bg);
+  }
+  &:focus{
+    outline: none;
+  }
+}
+
+</style>
