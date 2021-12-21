6104011646e2cfd80ff34221a0060e1cbc4c14ef
chaos430 <1192318530@qq.com>
2021-12-21 22:13:03 +0800

    创建 button-group

---
+++ src/button-group.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="l-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.l-button-group{
+  display: inline-flex;
+  vertical-align: middle;
+  > .l-button{
+    border-radius: 0;
+    margin-left: -1px;
+    &:first-child{
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child{
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover{
+      position: relative;
+      z-index:1;
+    }
+  }
+}
+</style>
