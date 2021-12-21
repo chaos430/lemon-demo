4784f5bc74c62e5e229cc1dca289ef14efd600c3
chaos430 <1192318530@qq.com>
2021-12-21 21:25:43 +0800

    将svg代码整理到icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg  class="l-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  props:['name'],
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.l-icon{
+  width: 1em;height:1em;
+}
+</style>
